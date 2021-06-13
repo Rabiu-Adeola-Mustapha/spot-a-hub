d7cb08d9e5693c04989af9e231d9c6c11e2036f3
TobbyJay <umohjunior96@gmail.com>
2021-06-13 15:59:06 +0100

    Setup Unit Of Work complete

---
+++ Repository/HubbRepository.cs
@@ -0,0 +1,51 @@
+﻿
+using Entities;
+using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Repository.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    internal class HubbRepository : RepositoryBase<Hubb>, IHubbRepository
+    {
+        public HubbRepository(AppDbContext context) : base(context)
+        {
+
+        }
+        public void CreateHubb(Hubb hubb)
+        {
+            Create(hubb);
+        }
+
+        public void DeleteSingleHubbId(Guid id)
+        {
+            DeleteSingleHubbId(id);
+        }
+
+      
+
+        public void UpdateHubb(Hubb hubb)
+        {
+            Update(hubb);
+        } 
+        
+        public async Task<Hubb> GetHubbById(Guid id, bool trackchanges)
+        {
+            var getHub = await FindByCondition(h => h.HubbId == id, trackchanges)
+                .AsNoTracking()              
+                .SingleOrDefaultAsync();
+
+            if (getHub == null)
+            {
+                return null;
+            }
+
+            return getHub;
+        }
+    }
+}
